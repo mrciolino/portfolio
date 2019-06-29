e1a0e2e258259bb4f5796f00f259d643a8939d2a
Matthew Ciolino <mrciolino@comcast.net>
2019-06-29 16:25:46 -0500

    port

--- app.py
+++ app.py
@@ -56,5 +56,7 @@ def predict_tags():
     return jsonify(result=result)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
